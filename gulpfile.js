ece8038592adae7ec3396bc929688a5af5431cbe
jrhicks <jrhicks@gmail.com>
2014-05-16 22:11:23 -0500

    First commit.

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,55 @@ var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var server = require('./gulpfile_server.js');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+    css: ['./www/css/**/*.css'],
+    js: ['./www/js/**/*.js'],
+    html: ['./www/index.html', './www/templates/**/*.html'],
+    sass: ['./scss/**/*.scss']
 };
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    gulp.src('./scss/ionic.app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(server.reload())
+        .on('end', done);
+});
+
+gulp.task('js', function(done) {
+    gulp.src(paths.js)
+        .pipe(server.reload())
+        .on('end', done);
+});
+
+gulp.task('css', function(done) {
+    gulp.src(paths.css)
+        .pipe(server.reload())
+        .on('end', done);
+});
+
+gulp.task('html', function(done) {
+    gulp.src(paths.html)
+        .pipe(server.reload())
+        .on('end', done);
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.css, ['css']);
+    gulp.watch(paths.html, ['html']);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['watch', 'server']);
+
+gulp.task('server', function() {
+    server.run();
+});
